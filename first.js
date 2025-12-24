02daceb2f630599c8ce396f9b819195c5dcf8dc0
sadia <s95351781@gmail.com>
2025-12-24 08:11:41 -0800

    message

--- first.js
+++ first.js
@@ -1,2 +1,14 @@
-fullName="sadia khan baloch";
-console.log(fullName);
+/*const product ={
+    title:"ball pen",
+    rating:4,
+    offer:5,
+    price:270,
+};
+console.log(product);*/
+/*const profile={
+    userName:"sadiakhan",
+    isFollow:false,
+    follower:234,
+    following:333,
+};
+console.log(profile);*/
---
+++ script.js
@@ -0,0 +1,97 @@
+//let a=5;
+//let b=10;
+//console.log("a=", a," & b=", b);
+//arithmatic operators
+/*console.log("a+b=", a+b);
+console.log("a-b=", a-b); 
+console.log("a*b=", a*b);
+console.log("a/b=", a/b);
+console.log("a%b=", a%b);
+console.log("a**b=", a**b);*/
+//unary operators
+/*console.log("++a=",++a);
+console.log("a=",a);
+
+console.log("a--=",a--);
+console.log("a=",a);*/
+//assignment operators
+/*let a=5;
+let b="6";
+a += 5
+console.log("a=",a)
+a -= 5
+console.log("a=",a)
+a *= 5
+//console.log("a=",a)
+a /= 5
+console.log("a=",a)*/
+//comparison operator
+//let a=5;
+//let b="6";
+//console.log("a==b",a==b);
+
+//logical operators
+/*let a=6;
+let b=8;
+
+console.log("!(a>b)=" ,!(a>b));*/
+
+//conditionals
+/*let age=29;
+
+if(age>=18 ){
+   console.log("You can vote");
+}
+else{
+    console.log("You cannot vote");
+}*/
+
+
+/*let mode="yellow"
+let color;
+if(mode==="dark"){
+    color="black";
+}else if(mode==="pink"){
+    color="pink";
+}else if(mode==="green"){
+    color="green";
+}else{
+    color="white";
+}
+console.log(color);*/
+
+/*let age=12 ;
+ age>=18?console.log( "adult"):console.log( " not adult");*/
+
+ //practice q1
+
+ /*let num=prompt("Enter a number");
+ console.log(num);
+
+ if(num%5===0){
+    console.log(num,"is a multiple of 5");
+ }else{
+    console.log(num,"is Not a multiple of 5");
+ }*/
+g
+
+    //practice q2
+
+let score=20;
+let grade;
+if(score>=90 && score<=100){
+    grade="A";
+}
+else if(score>=70 && score<=89){
+    grade="B";
+}
+else if(score>=60 && score<=69){
+    grade="C";
+}
+else if(score>=50 && score<=59){
+    grade="D";
+}
+else{
+    grade="F";
+}
+console.log(grade)
