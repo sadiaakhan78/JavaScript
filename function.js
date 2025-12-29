b2a6fe67bcb27108fc7fe4d250d10cd095efdbe4
sadia <s95351781@gmail.com>
2025-12-28 21:26:40 -0800

    message

--- function.js
+++ function.js
@@ -1,6 +1,8 @@
-/*function sum (x,y){
+function sum (x,y){
     s=x+y;
+    console.log("before return");
     return s;
+    console.log("before return");
 }
 let val=sum(5,8);
-console.log(val);*/
+console.log(val);
